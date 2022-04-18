5d3e01623a3f1f364af37e7f7f5520f25c1e6e9f
Sergioir <sergieteir@hotmail.com>
2022-04-18 16:48:17 +0200

    Add files via upload

---
+++ EnemyX.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = GameObject.FindWithTag("SpawnManager").GetComponent<SpawnManagerX>().speedEn;
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal=GameObject.FindWithTag("PlayerGoal");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+             GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Enemy");
+                       for (int i = 0; i < enemigos.Length; i++)
+                       {
+                           Destroy(enemigos[i]);
+                       }
+                        GameObject.FindWithTag("SpawnManager").GetComponent<SpawnManagerX>().SpawnEnemyWave(GameObject.FindWithTag("SpawnManager").GetComponent<SpawnManagerX>().waveCount);
+//Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+           // Destroy(gameObject);
+           GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Enemy");
+           for (int i = 0; i < enemigos.Length; i++)
+           {
+               Destroy(enemigos[i]);
+           }
+            GameObject.FindWithTag("SpawnManager").GetComponent<SpawnManagerX>().SpawnEnemyWave(GameObject.FindWithTag("SpawnManager").GetComponent<SpawnManagerX>().waveCount);
+
+        }
+
+    }
+
+}
---
+++ PlayerControllerX.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float speed = 2f;
+    private GameObject focalPoint;
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 8;
+    public ParticleSystem turboSmoke;
+    private float normalStrength = 10; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 24f;turboSmoke.Play();}else{
+            speed = 7f;
+        }
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed ); 
+
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, 0.2f, 0);
+
+    }
+
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer =  other.gameObject.transform.position-transform.position;
+            awayFromPlayer = awayFromPlayer.normalized;
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+
+
+        }
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Enemy");
+            for (int i = 0; i < enemigos.Length; i++)
+            {
+                Destroy(enemigos[i]);
+            }
+            GameObject.FindWithTag("SpawnManager").GetComponent<SpawnManagerX>().SpawnEnemyWave(GameObject.FindWithTag("SpawnManager").GetComponent<SpawnManagerX>().waveCount);
+        } 
+    }
+
+
+
+}
